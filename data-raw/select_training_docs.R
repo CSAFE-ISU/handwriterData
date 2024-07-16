f82e1727852dabf74f02cf53364ba13003af12b2
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-07-16 10:58:52 -0500

    Added template training docs to data-raw folder
    Initially, I planned to put the training docs in inst/extdata so that users could access them, but the images are too big. Instead, I put them in the data-raw folder which will be available to users on GitHub but not in the CRAN package.

--- data-raw/select_training_docs.R
+++ data-raw/select_training_docs.R
@@ -1,24 +1,20 @@
-## code to prepare `template` dataset goes here
+# Randomly select 100 CSAFE Handwriting Database London Letters for training a
+# cluster template
 
 library(dplyr)
 
 main_dir <- "/Users/stephanie/Documents/handwriting_datasets/CSAFE_Handwriting_Database"
-extdata_dir <- "inst/extdata/template/data/template_docs"
+template_docs <- "data-raw/template_docs"
 
 # make data frame
 df <- data.frame("folder" = list.files(main_dir, full.names = TRUE))
 df$writer <- basename(df$folder)
 df$doc <- file.path(main_dir, df$writer, paste0(df$writer, "_s01_pLND_r01.png"))
-df$extdata <- file.path(extdata_dir, paste0(df$writer, "_s01_pLND_r01.png"))
+df$template_doc <- file.path(template_docs, paste0(df$writer, "_s01_pLND_r01.png"))
 
 # select writers for template training
 set.seed(100)
 df <- df %>% dplyr::slice_sample(n=100, replace = FALSE)
 
 # copy training docs to extdata
-file.copy()
-
-
-
-# template <- readRDS(file.path("data-raw", "template.rds"))
-# usethis::use_data(template, overwrite = TRUE)
+file.copy(df$doc, df$template_doc)
