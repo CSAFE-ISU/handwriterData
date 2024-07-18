3a395bf0a3508ca17625a452e1cf0b7578ebcfb1
Stephanie Reinders <reinders.stephanie@gmail.com>
2024-07-18 16:04:14 -0500

    Created new `templateK40` from LND and WOZ prompts
    The `templateK40` created entirely from LND prompts only had 24 clusters. I made a new template from 50 LND and 50 WOZ prompts for better shape diversity. The new template has 40 clusters.
    
    The full updated template is saved in data-raw and the updatedated template minus `template_graphs` is saved in data.

--- data-raw/select_template_training_docs.R
+++ data-raw/select_template_training_docs.R
@@ -9,12 +9,20 @@ template_docs <- "data-raw/template_docs"
 # make data frame
 df <- data.frame("folder" = list.files(main_dir, full.names = TRUE))
 df$writer <- basename(df$folder)
-df$doc <- file.path(main_dir, df$writer, paste0(df$writer, "_s01_pLND_r01.png"))
-df$template_doc <- file.path(template_docs, paste0(df$writer, "_s01_pLND_r01.png"))
 
-# select writers for template training
-set.seed(100)
+# select 100 writers for template training
+set.seed(10)
 df <- df %>% dplyr::slice_sample(n=100, replace = FALSE)
+
+# get London Letters from first 50 writers and Wizard of Oz from the other 50
+df$doc <- rep(NA, 50)
+df$doc[1:50] <- file.path(main_dir, df$writer, paste0(df$writer, "_s01_pLND_r01.png"))[1:50]
+df$doc[51:100] <- file.path(main_dir, df$writer, paste0(df$writer, "_s01_pWOZ_r01.png"))[51:100]
+
+# format file path for template_docs folder
+df$template_doc <- file.path(template_docs, basename(df$doc))
+
+df$template_doc[]
 
 # copy training docs to extdata
 file.copy(df$doc, df$template_doc)
---
+++ scratch.R
@@ -0,0 +1,1 @@
+sample_writers <- function()
